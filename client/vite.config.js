79c0e0fb560b046b3c9e42d4a8002be95a6aef7e
Sourav Sharma <46624600+souravsharma121@users.noreply.github.com>
2024-05-23 02:03:47 +0530

    Update and rename vite.config.mjs to vite.config.js

---
+++ client/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "https://walkytalky-r0fb.onrender.com",
+    },
+  },
+  plugins: [react()],
+});
